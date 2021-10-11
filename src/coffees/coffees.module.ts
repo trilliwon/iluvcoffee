7dbb55407d0d6d86adf719a0646babadc9551dc6
Won <trilliwon@gmail.com>
2021-10-12 08:14:49 +0900

    finnished app configuration

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
-
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+import { Event } from 'src/events/entities/event.entity';
+import { ConfigModule } from '@nestjs/config';
+import coffeesConfig from './config/coffees.config';
+@Module({
+	imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event]), ConfigModule.forFeature(coffeesConfig)],
+	controllers: [CoffeesController],
+	providers: [CoffeesService],
+	exports: [CoffeesService],
+})
 export class CoffeesModule { }
